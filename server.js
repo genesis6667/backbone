04d04236939b8d55dcfc5cad62f84144e4f4284d
Abhirup Chowdhury <abchowdhury@DELC02QP581G8WN.sea.corp.expecn.com>
2016-04-25 16:35:04 +0530

    node js app

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ http.createServer( function (request, response) {
       // Send the response body 
       response.end();
    });   
-}).listen(8081);
+}).listen(process.env.PORT || 5000);
 
 // Console will print the message
 console.log('Server running at http://127.0.0.1:8081/');
